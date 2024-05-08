73af735be3b831db95deeec241fd983771785c02
Nhat Tien <ledangnhattien04@gmail.com>
2024-05-08 14:54:09 +0700

    feat: change icon in nvim web devicon

--- nvim/lua/plugins/nvim-web-devicons.lua
+++ nvim/lua/plugins/nvim-web-devicons.lua
@@ -1,1 +1,20 @@
-return { "nvim-tree/nvim-web-devicons", lazy = true }
+return {
+	"nvim-tree/nvim-web-devicons",
+	lazy = true,
+	config = function()
+		require("nvim-web-devicons").set_icon({
+			php = {
+				icon = "󰌟",
+				color = "#a074c4",
+				cterm_color = "140",
+				name = "Php",
+			},
+			["ts"] = {
+				icon = "󰛦",
+				color = "#519aba",
+				cterm_color = "74",
+				name = "TypeScript",
+			},
+		})
+	end,
+}
