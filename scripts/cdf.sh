3c4e508569d1ba81421e138ab09d7f0a21ddd4b9
Nhat Tien <ledangnhattien04@gmail.com>
2025-06-15 14:31:48 +0700

    chore: update cdf.sh become more correctly

--- scripts/cdf.sh
+++ scripts/cdf.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
 ignore_dirs=(
-  # .git
-  # .cache
-  # .venv
-  # .svelte-kit
   node_modules
   target
   dist
   build
-  ".*"
 )
 
 prune_expr=""
+
+# Ignore all dot folder, but exclude the folder in dot_exclude
+if true; then # Change true to false to disable
+
+  dot_exclude=(
+    .local
+  )
+
+  prune_expr+=" -path \"*/.*\""
+
+  for dir in "${dot_exclude[@]}"; do
+    prune_expr+=" ! -path \"*/${dir}\" ! -path \"*/${dir}/*\""
+  done
+
+  prune_expr+=" -o"
+fi
+# ---------------------------
 
 for dir in "${ignore_dirs[@]}"; do
   prune_expr+=" -path \"*/${dir}\" -o"
@@ -20,7 +32,8 @@ done
 
 prune_expr=$(echo "${prune_expr}" | sed "s/ -o$//")
 
-dir=$(eval "find "${1:-.}" \( "${prune_expr}" \) -prune -o -type d -print" 2>/dev/null | fzf )
+
+dir=$(eval "find \"${1:-.}\" \( $prune_expr \) -prune -o -type d -print" 2>/dev/null | fzf )
 
 if [[ -n "$dir" ]]; then
   cd "$dir" || echo "Failed to cd into $dir"
