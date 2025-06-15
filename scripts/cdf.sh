eca6073ff4c39b09c176b2444e190b5235845e8f
Nhat Tien <ledangnhattien04@gmail.com>
2025-06-15 10:16:43 +0700

    chore: update cdf.sh

--- scripts/cdf.sh
+++ scripts/cdf.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
-dir=$(find "${1:-.}" \( -name .git \
-  -o -name node_modules \
-  -o -name target \
-  -o -name dist \
-  -o -name build \
-  -o -name .cache \
-  -o -name .venv \) -prune -o -type d -print 2>/dev/null | fzf )
+ignore_dirs=(
+  # .git
+  # .cache
+  # .venv
+  # .svelte-kit
+  node_modules
+  target
+  dist
+  build
+  ".*"
+)
+
+prune_expr=""
+
+for dir in "${ignore_dirs[@]}"; do
+  prune_expr+=" -path \"*/${dir}\" -o"
+done
+
+prune_expr=$(echo "${prune_expr}" | sed "s/ -o$//")
+
+dir=$(eval "find "${1:-.}" \( "${prune_expr}" \) -prune -o -type d -print" 2>/dev/null | fzf )
+
 if [[ -n "$dir" ]]; then
   cd "$dir" || echo "Failed to cd into $dir"
 fi
