8b07ebf2af94c9528dee5d2e05f6292e38262a6e
Nhat Tien <ledangnhattien04@gmail.com>
2025-06-28 02:25:42 +0700

    feat(nvim): new plugin

---
+++ nvim/lua/plugins/coerce-nvim.lua
@@ -0,0 +1,12 @@
+return {
+   "gregorias/coerce.nvim",
+   tag = 'v4.1.0',
+   config = true,
+   opts = {
+      default_mode_keymap_prefixes = {
+         normal_mode = "<leader>uc",
+         motion_mode = "<leader>uc",
+         visual_mode = "<leader>uc",
+      },
+   }
+}
